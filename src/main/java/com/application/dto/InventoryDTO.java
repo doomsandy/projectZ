67d37fd420bfabe51212d753eb7b2dff3fad2457
doomsandy <doomsandyy@gmail.com>
2023-07-31 14:50:23 +0300

    delete method fromInventory

--- src/main/java/com/application/dto/InventoryDTO.java
+++ src/main/java/com/application/dto/InventoryDTO.java
@@ -39,20 +39,6 @@ public class InventoryDTO {
         this.depreciation = depreciation;
     }
 
-    public static InventoryDTO fromInventory(Inventory inventory) {
-        int cost = inventory.getCost();
-        int depreciation = inventory.getDepreciation();
-        if (inventory instanceof Mouse) {
-            Mouse mouse = (Mouse) inventory;
-            return new MouseDTO(cost, depreciation, mouse.getSensorType());
-        }
-        if (inventory instanceof Chair) {
-            Chair chair = (Chair) inventory;
-            return new ChairDTO(cost, depreciation, chair.getColor(), chair.getType());
-         }
-        return new InventoryDTO(cost, depreciation);
-    }
-
     public static Set<InventoryDTO> fromInventories(Set<Inventory> inventories) {
         Set<InventoryDTO> inventoryDTOS = new HashSet<>();
         for (Inventory inventory : inventories) {
