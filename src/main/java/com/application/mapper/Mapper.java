8a92ba679e60802b909dfcc4e7b9b73ffe575d1d
doomsandy <doomsandyy@gmail.com>
2023-08-04 16:20:13 +0300

    -mapper

--- src/main/java/com/application/mapper/Mapper.java
+++
@@ -1,9 +0,0 @@
-package com.application.mapper;
-
-import com.application.dto.InventoryDTO;
-import com.application.entity.Inventory;
-
-public interface Mapper<E extends Inventory, D extends InventoryDTO> {
-    D toDto(E entity);
-    E toEntity(D dto);
-}
